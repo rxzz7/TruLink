0ca5d7ae4acb0da0613df1d2d92abe96c6698261
Raj <razsk0769@gmail.com>
2026-09-16 12:58:01 +0530

    Initial commit

---
+++ src/main/java/com/tru_link/Trulink/TrulinkApplication.java
@@ -0,0 +1,13 @@
+package com.tru_link.Trulink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrulinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrulinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tru_link/Trulink/TrulinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tru_link.Trulink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrulinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
